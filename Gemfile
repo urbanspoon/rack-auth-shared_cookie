31994833f2fe6fdadfd4c8e24f903758146b27eb
Grant Rodgers <grantr@gmail.com>
2011-09-07 12:25:31 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "http://rubygems.org"
+
+# Specify your gem's dependencies in rack-auth-shared_cookie.gemspec
+gemspec
---
+++ lib/rack-auth-shared_cookie.rb
@@ -0,0 +1,9 @@
+require "rack-auth-shared_cookie/version"
+
+module Rack
+  module Auth
+    module Sharedcookie
+      # Your code goes here...
+    end
+  end
+end
