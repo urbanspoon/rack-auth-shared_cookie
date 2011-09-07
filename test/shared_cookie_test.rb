9c89c071ccdbc65bc9ce5b76bfb31b4dd10aa38c
Grant Rodgers <grantr@gmail.com>
2011-09-07 12:58:10 -0700

    setup minitest

---
+++ test/shared_cookie_test.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'minitest/spec'
+require 'minitest/autorun'
+
+describe Rack::Auth::SharedCookie do
+  it 'should be true' do
+    nil.must_be_nil
+  end
+end
