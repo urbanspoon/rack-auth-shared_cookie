99c518b74986b4eb52faa98c926a4c5a2ce4021a
Grant Rodgers <grantr@gmail.com>
2011-09-19 14:59:19 -0700

    add tests

--- test/shared_cookie_test.rb
+++ test/shared_cookie_test.rb
@@ -1,14 +1,42 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
 
 describe Rack::Auth::SharedCookie do
+  include Rack::Test::Methods
 
-  it 'should read cookie'
-  it 'should write cookie'
-  it 'should reject cookies with an invalid signature'
+  let(:secret) { '1' }
+  let(:verifier) { ActiveSupport::MessageVerifier.new(secret) }
+
+  def app
+    @target = lambda { [200, { }, "Test app"] }
+  
+    Rack::Auth::SharedCookie.new(@target, :secret => secret)
+  end
+
+  it 'should read cookie' do
+    set_cookie("auth_token=#{verifier.generate('AUTH_USER' => 'a')};")
+    get '/foo'
+    last_request.env['rack.auth.user'].must_equal 'a'
+  end
+
+  it 'should write cookie' do
+    get '/foo', {}, {'rack.auth.user' => 'a'}
+    last_response.headers['Set-Cookie'].must_include verifier.generate('AUTH_USER' => 'a')
+  end
+
+  it 'should reject cookies with an invalid signature' do
+    set_cookie("auth_token=#{verifier.generate('AUTH_USER' => 'a').succ};")
+    get '/foo'
+    last_request.env['rack.auth.user'].must_be_nil
+  end
+
+  it 'should write cookies with the shared domain' do
+    get 'http://www.example.org/foo', {}, {'rack.auth.user' => 'a'}
+    last_response.headers['Set-Cookie'].must_include "domain=.example.org"
+  end
 
   describe 'cookie_domain' do
     def generate(options={})
-      Rack::Auth::SharedCookie.new(nil, options.merge(:secret => '1'))
+      Rack::Auth::SharedCookie.new(nil, options.merge(:secret => secret))
     end
 
     it 'should return nil if shared_domain is false' do
